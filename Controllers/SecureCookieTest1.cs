373921c3df6a13eb74b4c7187d2b962b1a27a716
benshmuely <ben.shmuely@whitesourcesoftware.com>
2021-03-07 18:29:27 +0200

    comment

--- Controllers/SecureCookieTest1.cs
+++ Controllers/SecureCookieTest1.cs
@@ -1,7 +1,29 @@
-﻿namespace WebFox.Controllers
+﻿using System.Net;
+using System.Security.Cryptography;
+using Microsoft.AspNetCore.Http;
+
+namespace WebFox.Controllers
 {
     public class SecureCookieTest1
     {
         // HttpCookie myCookie = new HttpCookie("Sensitive cookie");
+        public void DoPost(HttpResponse response, HttpRequest request)
+        {
+            DoGet(response, request);
+        }
+
+        public void DoGet(HttpResponse response, HttpRequest request)
+        {
+            Unsafe(response, request);
+        }
+
+        public void Unsafe(HttpResponse response, HttpRequest request)
+        {
+            string password = "p-" + RandomNumberGenerator.GetInt32(200000000, 2000000000);
+            Cookie cookie = new Cookie("password",password);
+            cookie.Path = "/";
+            cookie.Domain = "";
+            cookie.Comment = "Cookie Description";
+        }
     }
 }
